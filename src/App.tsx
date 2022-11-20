0a142db022a9d0b1b9c4077f9589a654a5470782
gottu91 <gotuu91.afl@gmail.com>
2022-11-20 10:23:34 +0900

    関数の型/オプショナルチェイング/型のコンポーネント化を練習しました。

--- src/App.tsx
+++ src/App.tsx
@@ -1,84 +1,42 @@
 import { useEffect, useState } from "react";
+import { ListItem } from "./ListItem";
+import type { User } from "./types/user";
 
-function App() {
-    // let testVariable: boolean = true;
-    // console.log(testVariable);
+const memberList = [
+    {
+        'id': 1,
+        'name': '主田',
+        'age': 24,
+        'personalColor': 'blue',
+    },
+    {
+        'id': 2,
+        'name': '先岡',
+        'age': 28,
+        'personalColor': 'pink',
+    },
+    {
+        'id': 3,
+        'name': '後藤',
+        'age': 23,
+        'personalColor': 'green',
+        'hobbies': ['game', 'soccer'],
+    },
+]
 
-    // const testArray: Array<number> = [2, 2, 3];
-    // console.log(testArray);
+export const App = () => {
+    const [users, setUsers] = useState<User[]>([]);
 
-    // const testArray2: number[] = [2, 2, 2, 2];
-    // console.log(testArray2);
-
-    // const testNull: null = null;
-    // console.log(testNull);
-
-    // const testUndefind: undefined = undefined;
-    // console.log(testUndefind);
-
-    // const testAny: any = [2,3,4];
-    // console.log(testAny);
-
-    // const testFunc = (text: string): void => {
-    //     console.log(text);
-    // }
-    // testFunc('こんにちは');
-
-    // const obj: {name: string, age: number} = {
-    //     name: 'goto',
-    //     age: 31,
-    // }
-    // console.log(obj.name);
-    // console.log(obj.age);
-
-    // const obj: {name: string} & {age: number} = {
-    //     name: 'goto',
-    //     age: 31,
-    // }
-    // console.log(obj.name);
-    // console.log(obj.age);
-
-    // type typeA = {
-    //     name: string,
-    //     age: number,
-    // }
-    // type typeB = {
-    //     name: string,
-    //     bool: boolean,
-    // }
-    // // type typeC = typeA & typeB;
-    // const obj: typeA & typeB = {
-    //     name: 'goto',
-    //     age: 31,
-    //     bool: true,
-    // }
-    // console.log(obj.name);
-    // console.log(obj.age);
-    // console.log(obj.bool);
-
-    // const text: string | number = 'こんにちは';
-    // console.log(text);
-
-    // type CustomType<T> = {
-    //     val: T,
-    // }
-    // const obj: CustomType<string> = {
-    //     val: 'おはよう',
-    // }
-    // console.log(obj.val);
-
-    const [count, setCount] = useState<number>(0);
+    // 画面表示時にユーザー情報取得
     useEffect(() => {
-        console.log(count);
-        setCount((prevNum) => prevNum + 1);
-    }, []);
-    console.log(count);
+        setUsers(memberList);
+    }, [])
 
     return (
-        <>
-            <p>typescript</p>
-        </>
-    );
+        <div>
+            {users.map((user) => (
+                <ListItem id={user.id} name={user.name} age={user.age} personalColor={user.personalColor} hobbies={user.hobbies} />
+            ))}
+        </div>
+    )
 }
-
-export default App;
